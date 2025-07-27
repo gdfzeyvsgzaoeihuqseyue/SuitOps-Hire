f20048bb455e48a9db5ddc46fecf7ca29f680ceb
SteveAsterAfovo <steveasterafovo@gmail.com>
2025-07-27 05:34:54 +0100

    add gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   modules: [
+    ['nuxt-gtag', {
+      id: process.env.GTAG_ID || 'G-EQ1NZNBVB4', 
+      enabled: process.env.NODE_ENV === 'production'
+    }],
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     '@vueuse/nuxt',
